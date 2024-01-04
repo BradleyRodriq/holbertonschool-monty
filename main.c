791b07794b8fcb8bcd73aaddc4f6df0e207de2f1
BradleyRodriq <bradley.rodriguez2@yahoo.com>
2024-01-04 00:21:02 -0400

    another error typo fix

--- main.c
+++ main.c
@@ -25,7 +25,7 @@ int main(int argc, char *argv[])
 
 	if (file == NULL)
 	{
-		printf("Error: Can't open file %s\n", argv[1]);
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		fclose(file);
 		return (EXIT_FAILURE);
 	}
