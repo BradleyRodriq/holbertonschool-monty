ca98f5b45b1488c132c7304f11eb86bfb7b9afb3
BradleyRodriq <bradley.rodriguez2@yahoo.com>
2024-01-04 00:04:41 -0400

    added free stack to .h file

--- monty.h
+++ monty.h
@@ -34,5 +34,6 @@ typedef struct instruction_s
 void push(stack_t **stack, int value, unsigned int line_number);
 void pall(stack_t **stack, unsigned int line_number);
 void process_line(stack_t **stack, const char *line, unsigned int line_number);
+void free_stack(stack_t *stack);
 
 #endif
