3f833dd192a8d6cecda60fbe615bf8b2becd318b
Robert Santana <123roabel@gmail.com>
2024-01-04 18:52:17 -0400

    create nop

---
+++ nop.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+
+/**
+ * nop - Does nothing.
+ * @stack: Pointer to the stack.
+ * @line_number: Line number in the Monty file.
+ */
+void nop(Stack **stack, unsigned int line_number)
+{
+    (void)stack;
+    (void)line_number;
+}
