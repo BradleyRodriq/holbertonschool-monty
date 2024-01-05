3794c4a8df3f7cc7e3040f34202f03fdb2ff15b1
BradleyRodriq <bradley.rodriguez2@yahoo.com>
2024-01-05 13:51:21 -0400

    added sub function

--- add.c
+++ add.c
@@ -12,7 +12,6 @@ void add(stack_t **stack, unsigned int line_number)
 		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
-
 	(*stack)->next->n += (*stack)->n;
 	pop(stack, line_number);
 }
--- main.c
+++ main.c
@@ -92,6 +92,10 @@ void process_line(stack_t **stack, const char *line, unsigned int line_number)
 		{
 			nop(stack, line_number);
 		}
+		else if (strcmp(opcode, "sub") == 0)
+		{
+			sub(stack, line_number);
+		}
 		else
 		{
 			fprintf(stderr, "L%u: unknown instruction %s\n", line_number, opcode);
