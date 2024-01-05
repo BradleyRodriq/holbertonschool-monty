d4f04233a563074ee5b742456df27c7e3df4a7e9
BradleyRodriq <bradley.rodriguez2@yahoo.com>
2024-01-05 14:14:06 -0400

    added free to pop function

--- pop.c
+++ pop.c
@@ -21,6 +21,7 @@ int pop(stack_t **stack, unsigned int line_number)
 	else
 	{
 		fprintf(stderr, "L%u: can't pop an empty stack\n", line_number);
+		free_stack(*stack);
 		exit(EXIT_FAILURE);
 	}
 }
